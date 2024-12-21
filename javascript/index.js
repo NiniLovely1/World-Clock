6a25e4149157692810aabb4874638bc0ca78600f
NiniLovely1 <ninitaylor78@gmail.com>
2024-12-21 11:14:49 -0700

    added javascript and updated html

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,23 @@
+setInterval(function () {
+  let coloradoSpringsElement = document.querySelector("#colorado-springs");
+  let coloradoSpringsDateElement =
+    coloradoSpringsElement.querySelector(".date");
+  let coloradoSpringsTimeElement =
+    coloradoSpringsElement.querySelector(".time");
+  let coloradoSpringsTime = moment().tz("America/Denver");
 
+  coloradoSpringsDateElement.innerHTML = moment().format("MMMM Do YYYY");
+  coloradoSpringsTimeElement.innerHTML = `${coloradoSpringsTime.format(
+    "hh:mm:ss"
+  )} <small>${coloradoSpringsTime.format("A")}</small>`;
+
+  let parisElement = document.querySelector("#paris");
+  let parisDateElement = parisElement.querySelector(".date");
+  let parisTimeElement = parisElement.querySelector(".time");
+  let parisTime = moment().tz("Europe/Paris");
+
+  parisDateElement.innerHTML = moment().format("MMMM Do YYYY");
+  parisTimeElement.innerHTML = `${parisTime.format(
+    "hh:mm:ss"
+  )} <small>${parisTime.format("A")}</small>`;
+}, 1000);
